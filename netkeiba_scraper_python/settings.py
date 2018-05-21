bb1f0560a273dfcf7e55cb1deece9abe1e238f34
wdy06 <mituhashitakashi@gmail.com>
2018-05-21 18:36:40 +0900

    update

--- netkeiba_scraper_python/settings.py
+++ netkeiba_scraper_python/settings.py
@@ -27,12 +27,12 @@ CONNECTION_STRING = "{drivername}:///{db_name}".format(
 ROBOTSTXT_OBEY = True
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-CONCURRENT_REQUESTS = 8
+#CONCURRENT_REQUESTS = 8
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://doc.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 2
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
